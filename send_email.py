e2c018b25b3b0c619ce13d9d0b589c8efd5b3bcc
Tharun <tharunvijay836@gmail.com>
2024-02-28 13:20:19 +0530

    Initial commit

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "tharunapp8@gmail.com"
+    password = "fpsl cjgv fzqz wyyp"
+
+    receiver = "tharunapp8@gmail.com"
+    context = ssl.create_default_context()
+
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+#vwja mtnn tlob iggd
